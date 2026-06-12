edb61bed454ea3d670ab64a361ef0e3a0790047c
vktrrdk <accounts@viktorrudko.de>
2026-06-12 14:41:10 +0000

    fonts

--- wiki/mathjax.js
+++ wiki/mathjax.js
@@ -1,13 +1,14 @@
 window.MathJax = {
-      tex: {
-          inlineMath: [["$", "$"], ["\\(", "\\)"]],
-          displayMath: [["$$", "$$"], ["\\[", "\\]"]],
-          processEscapes: true,
-          processEnvironments: true
-      },
-      options: {
-          // Remove or comment out the processHtmlClass line to let MathJax
-          // process everything unless specifically told not to.
-           processHtmlClass: "arithmatex"
-      }
-  };
+    loader: {
+        paths: { 'mathjax-stix2': '/assets/mathjax' }
+    },
+    tex: {
+        inlineMath: [["$", "$"], ["\\(", "\\)"]],
+        displayMath: [["$$", "$$"], ["\\[", "\\]"]],
+        processEscapes: true,
+        processEnvironments: true
+    },
+    options: {
+        processHtmlClass: "arithmatex"
+    }
+};
