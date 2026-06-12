e140075c3aababab17b130a80de8411f1e22617b
vktrrdk <accounts@viktorrudko.de>
2026-06-12 14:47:18 +0000

    font-path

--- wiki/mathjax.js
+++ wiki/mathjax.js
@@ -1,14 +1,14 @@
 window.MathJax = {
     loader: {
-        paths: { 'mathjax-stix2': '/assets/mathjax' }
+        load: ['[mathjax-stix2]/chtml'],
+        paths: { 'mathjax-stix2': '/wiki/assets/mathjax' }
     },
+    chtml: { font: 'mathjax-stix2' },
     tex: {
         inlineMath: [["$", "$"], ["\\(", "\\)"]],
         displayMath: [["$$", "$$"], ["\\[", "\\]"]],
         processEscapes: true,
         processEnvironments: true
     },
-    options: {
-        processHtmlClass: "arithmatex"
-    }
+    options: { processHtmlClass: "arithmatex" }
 };
